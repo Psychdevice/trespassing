e02892b91040ba2e4cc57d60fe23a45595ad692e
rexhunter99@gmail.com <rexhunter99@gmail.com@db0c01cf-33f4-d6d2-84bd-1f87e1761c80>
2013-05-29 12:45:29 +0000

    Forgot FStream.cpp... lol

---
+++ trunk/tpass/FStream.cpp
@@ -0,0 +1,42 @@
+
+#include "FStream.hpp"
+#include "Exception.hpp"
+#include <stdio.h>
+
+FStream::FStream( const char* p_name, FStreamAccess p_access )
+{
+	switch ( p_access )
+	{
+	case FACCESS_READ:
+		{
+			m_stream = (void*)fopen( p_name, "rb" );
+		}
+		break;
+
+	case FACCESS_WRITE:
+		{
+			m_stream = (void*)fopen( p_name, "wb" );
+		}
+		break;
+
+	case FACCESS_BOTH:
+		{
+			m_stream = (void*)fopen( p_name, "rb+" );
+		}
+		break;
+
+	default:
+		{
+			throw Exception( "FStream :: Incorrect access flag was given!" );
+		}
+		break;
+	}
+}
+
+FStream::~FStream()
+{
+	if ( !m_stream ) throw Exception( "FStream :: Invalid stream in ~FStream()!" );
+
+	fclose( (FILE*)m_stream );
+	m_stream = (void*)NULL;
+}
