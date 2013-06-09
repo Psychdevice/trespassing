71885c2c62a357add8fbb140a710a439f1aade4d
rexhunter99 <rexhunter99@gmail.com>
2013-06-09 03:45:21 +0000

    Commiting shaders...

---
+++ tpass/Game/data/shaders/simple-v.glsl
@@ -0,0 +1,24 @@
+// Vertex Shader - file "simple"
+
+#version 120
+
+attribute vec3 in_Position;
+attribute vec3 in_Color;
+
+void main(void)
+{
+        gl_Position = vec4(in_Position, 1.0);
+        gl_FrontColor = in_Color;
+}
+
+#version 140
+
+in  vec3 in_Position;
+in  vec3 in_Color;
+out vec3 ex_Color;
+
+void main(void)
+{
+        gl_Position = vec4(in_Position, 1.0);
+        ex_Color = in_Color;
+}
